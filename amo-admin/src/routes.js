f66dff94ff375aa43069d72e12c911df72764fe2
hoangkhanhson2000 <hoangkhanhson2000@gmail.com>
2022-04-04 09:56:58 +0700

    us-518/listAsset - [feature-926] resolve conflict for manage list Asset 2

--- amo-admin/src/routes.js
+++ amo-admin/src/routes.js
@@ -5,14 +5,22 @@ const AddEdit = React.lazy(() => import('./features/users/page/AddEdit'));
 const UserManager = React.lazy(() => import('./features/users/page/userList'));
 const CreateAsset = React.lazy(() => import('./features/asset/page/CreateAsset'));
 const ManageAsset = React.lazy(() => import('./features/asset/page/ManageAsset'));
-
+const CreateAsset = React.lazy(() =>
+    import("./features/asset/page/CreateAsset")
+);
+const EditAsset = React.lazy(() => import("./features/asset/page/EditAsset"));
 
 const routes = [
-    { path: '/home', exact: true, name: 'Home', component: Home },
-    { path: '/manageasset/createasset', name: 'Create Asset', component: CreateAsset },
-    { path: '/manageasset', name: 'Manage Asset', component: ManageAsset },
-    { path: '/manageuser/create',  name: 'Create User', component: AddEdit },
-    { path: '/manageuser', exact: true, name: 'User', component: UserManager },
+    { path: "/home", exact: true, name: "Home", component: Home },
+    {
+        path: "/manageasset/createasset",
+        name: "Create Asset",
+        component: CreateAsset,
+    },
+    { path: "/manageasset/editasset/:id", name: "Edit Asset", component: EditAsset },
+    { path: "/manageuser/create", name: "Create User", component: AddEdit },
+    { path: "/manageuser", exact: true, name: "User", component: UserManager },
 ];
 
 export default routes;
+
