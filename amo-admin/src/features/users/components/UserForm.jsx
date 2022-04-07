4b77609a89617c4f0fd6df4e8e49fb4e3fc4a795
Hùng Đinh Xuân <hungdinhxdev@gmail.com>
2022-04-07 06:56:05 +0000

    Merged PR 409: change validate rule first name, last name
    - bug-1201, fix layout header create new user
    - bug-1201, change validate rule first name, last name
    
    Related work items: #1201

--- amo-admin/src/features/users/components/UserForm.jsx
+++ amo-admin/src/features/users/components/UserForm.jsx
@@ -45,8 +45,16 @@ function UserForm(props) {
     Email: Yup.string()
       .required("This field is required.")
       .email("Invalid email"),
-    FirstName: Yup.string().required("This field is required."),
-    LastName: Yup.string().required("This field is required."),
+    FirstName: Yup.string()
+    .required("This field is required.")
+    .test("FirstName", "Invalid First Name, it must have max length less than or equal 100 character and contain character from A-Z, a-z, 0-9", (value) => {
+      return value.length <= 100 && /^[a-zA-Z0-9]+$/.test(value);
+    })
+    ,
+    LastName: Yup.string().required("This field is required.")
+    .test("LastName", "Invalid Last Name, it must have max length less than or equal 100 character and contain character from A-Z, a-z, 0-9", (value) => {
+      return value.length <= 100 && /^[a-zA-Z0-9]+$/.test(value);
+    }),
     Type: Yup.string().required("This field is required.").nullable(),
     DateOfBirth: Yup.string()
       .required("This field is required.")
