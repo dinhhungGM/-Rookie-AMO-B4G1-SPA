a59c61159e9f5b55c1f9ba7b9ac0023e7c11f228
huytung255 <61749223+huytung255@users.noreply.github.com>
2022-04-08 14:09:52 +0700

    develop, fix bug: disable deleting for assigned assets

--- amo-admin/src/features/asset/components/ManageAssetTable.jsx
+++ amo-admin/src/features/asset/components/ManageAssetTable.jsx
@@ -180,7 +180,7 @@ const ManageAssetTable = ({ listitem, onRefresh, params, setparams }) => {
             onClick={() => {
               handleDeleteAsset(row.original.id);
             }}
-            disabled={false}
+            disabled={stateArr[row.original.state] === "Assigned"}
           />
         </div>
       ),
